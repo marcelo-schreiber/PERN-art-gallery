0d8f43f1830b272e94e7ffc8a5c6f5afb4c6cd7a
Marcelo Augusto Rissette Schreiber <maraugrissch@gmail.com>
2020-09-30 18:02:39 -0300

    changed title

--- Frontend/src/utils/GetPosTouch.js
+++
@@ -1,8 +0,0 @@
-// Get the position of a touch relative to the canvas
-export const getTouchPos = (canvasDom, touchEvent) => {
-  var rect = canvasDom.getBoundingClientRect();
-  return {
-    posx: touchEvent.touches[0].clientX - rect.left,
-    posy: touchEvent.touches[0].clientY - rect.top,
-  };
-};
